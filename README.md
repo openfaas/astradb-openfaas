085f0a20056a4e6e3d93b4cf0808236deb13f1a2
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-09-14 11:04:24 +0100

    Add cycling example
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 Astra DB examples with OpenFaaS
 ===============================
 
-Examples:
+##Usage:
 
-* weekly-newsletter - using the Document SDK
+Read the article: [Serverless storage for your functions from the Datastax Astra DB](https://www.openfaas.com/blog/faas-storage-cassandra-astra/)
 
+Sign up for an account for [Astra DB and managed Cassandra](https://dtsx.io/2VYD4I4): 
+
+Examples in this repository:
+
+* [cycling](/cycling/) - Node.js example using Astra DB and the Cassandra Driver
+* [weekly-newsletter](/weekly-newsletter/) - Node.js for Astra DB using the Document SDK
+
+
+## License: 
+
+MIT
+
+### Contributions
+
+Subject to raising an issue, and having the suggestion approved by a maintainer.
---
+++ cycling/handler.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const { Client } = require("cassandra-driver");
+const fs = require("fs").promises
+
+module.exports = async (event, context) => {
+  const clientID = await fs.readFile("/var/openfaas/secrets/astra-clientid", "utf8")
+  const clientSecret = await fs.readFile("/var/openfaas/secrets/astra-clientsecret", "utf8")
+
+  const client = new Client({
+    cloud: {
+      secureConnectBundle: "/var/openfaas/secrets/astra-secure-connect",
+    },
+    credentials: {
+        username: clientID.trim(),
+        password: clientSecret.trim(),
+    },
+  });
+
+  await client.connect();
+
+  // Execute a query
+  const result = await client.execute("SELECT * FROM cycling.cyclist_name");
+
+  console.log(`Your cluster returned ${result.rowLength} row(s)`);
+  await client.shutdown();
+
+  return context
+    .status (200)
+    .succeed (result. rows)
+}
